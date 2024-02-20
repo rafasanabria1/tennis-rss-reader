5cb0caa44f956b58580671af64073934d0e49622
Rafael Sanabria Cruz <rafael.sanabria@es.logicalis.com>
2024-02-20 11:31:16 +0100

    Create skeleton to loading state

--- src/components/feed-loading.tsx
+++ src/components/feed-loading.tsx
@@ -1,3 +1,70 @@
 export default function FeedLoading() {
-  return <div>Cargando...</div>;
+  return (
+    <div className="animate-pulse" role="status">
+      <div className="">
+        <div className="mx-auto h-8 w-96 rounded-xl bg-fuchsia-950 opacity-60" />
+      </div>
+      <main className="my-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <div className="flex flex-col gap-2 rounded-lg border border-fuchsia-950 p-4">
+            <svg
+              aria-hidden="true"
+              className="h-32 w-full text-fuchsia-950 md:h-64 "
+              fill="currentColor"
+              viewBox="0 0 20 18"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+            </svg>
+
+            <div className="h-2 w-full rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="h-2 w-full rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="h-2 w-2/3 rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="mt-6 flex h-1 items-center justify-between">
+              <div className="h-1 w-32 rounded-xl bg-fuchsia-950 opacity-60" />
+              <div className="h-1 w-32 rounded-xl bg-fuchsia-950 opacity-60" />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 rounded-lg border border-fuchsia-950 p-4">
+            <svg
+              aria-hidden="true"
+              className="h-32 w-full text-fuchsia-950 md:h-64 "
+              fill="currentColor"
+              viewBox="0 0 20 18"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+            </svg>
+
+            <div className="h-2 w-full rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="h-2 w-full rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="h-2 w-2/3 rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="mt-6 flex h-1 items-center justify-between">
+              <div className="h-1 w-32 rounded-xl bg-fuchsia-950 opacity-60" />
+              <div className="h-1 w-32 rounded-xl bg-fuchsia-950 opacity-60" />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 rounded-lg border border-fuchsia-950 p-4">
+            <svg
+              aria-hidden="true"
+              className="h-32 w-full text-fuchsia-950 md:h-64 "
+              fill="currentColor"
+              viewBox="0 0 20 18"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+            </svg>
+
+            <div className="h-2 w-full rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="h-2 w-full rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="h-2 w-2/3 rounded-xl bg-fuchsia-950 opacity-60" />
+            <div className="mt-6 flex h-1 items-center justify-between">
+              <div className="h-1 w-32 rounded-xl bg-fuchsia-950 opacity-60" />
+              <div className="h-1 w-32 rounded-xl bg-fuchsia-950 opacity-60" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
